fec8a5c55e966d492cb4cf18d9b15050da3c87ed
renebisperink <66679652+renebisperink@users.noreply.github.com>
2021-08-24 17:45:51 +0200

    Add files via upload

---
+++ installtheme.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host "Please run this script with Administrator Rights"
+Invoke-WebRequest -Uri https://github.com/fireeye/commando-vm/archive/refs/heads/master.zip -OutFile ./master.zip
+unzip.exe master.zip 
+cd .\commando-vm-master
+
+.\install.ps1 -nochecks $true -profile_file .\Profiles\full.json
+
+cd ..
+
+Start-Process -Wait Set-WallPaper -Image “.\kiwa-commandovm.png”
